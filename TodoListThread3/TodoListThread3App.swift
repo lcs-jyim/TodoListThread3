32da36cae1d00c3161efc30a2e65853490aed07d
Josh Yim <jyim@lcs.on.ca>
2024-04-08 11:13:31 -0400

    Initial Commit

---
+++ TodoListThread3/TodoListThread3App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListThread3App.swift
+//  TodoListThread3
+//
+//  Created by junxi Yim on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct TodoListThread3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
