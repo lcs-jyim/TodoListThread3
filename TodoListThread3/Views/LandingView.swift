0e18829ce00263e255d6bd0ef432e4083fb6911e
Josh Yim <jyim@lcs.on.ca>
2024-04-10 12:05:40 -0400

    Can now mark item as being done or not by tapping.

--- TodoListThread3/Views/LandingView.swift
+++ TodoListThread3/Views/LandingView.swift
@@ -24,9 +24,17 @@ struct LandingView: View {
         NavigationView{
             
             VStack {
-                List(todos) { todo in
+                List($todos) { $todo in
                     
                     ItemView(currentItem: todo)
+                        .swipeActions {
+                            Button("delete", role:.destructive, action: {delete(todo)
+                            }
+                        )
+                    }
+                        .onTapGesture {
+                            todo.done.toggle()
+                        }
                     
                 }
                 .searchable(text: $searchText)
@@ -35,15 +43,27 @@ struct LandingView: View {
                     TextField("Enter a To-do item", text: $newItemDescription)
                     Button("ADD") {
                         // Add the new todo item
-                        
+                        creatToDo(withTitle: newItemDescription)
                     }
                     .font(.caption)
+                    .disabled(newItemDescription.isEmpty == true)
                 }
                 .padding(20)
             }
             .navigationTitle("To do")
         }
         
+    }
+    
+    //MARK: Fuctions
+    func creatToDo(withTitle title:String) {
+        let todo = TodoItem(title: title, done: false)
+        todos.append(todo)
+    }
+    func delete(_ todo: TodoItem){
+        todos.removeAll { currentItem in
+            currentItem.id == todo.id
+        }
     }
 }
 
